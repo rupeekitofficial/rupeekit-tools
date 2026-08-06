09fb9cd2def6a07644cba359b25a4157c545805e
rupeekitofficial <rupeekitofficial@gmail.com>
2026-08-06 08:33:46 +0530

    Include issue 45 articles in blog registry

--- data/all-blog-posts.ts
+++ data/all-blog-posts.ts
@@ -1,5 +1,6 @@
 import { blogPosts as coreBlogPosts } from './blog-posts';
 import { extraBlogPosts } from './extra-blog-posts';
+import { day4ComplianceBlogPosts } from './day4-compliance-blog-posts';
 import { CONSOLIDATED_BLOG_SLUGS } from '../lib/consolidated-routes';
 
 export type {
@@ -10,5 +11,8 @@ export type {
   BlogQuickAnswerLink,
 } from './blog-posts';
 
-export const blogPosts = [...extraBlogPosts, ...coreBlogPosts]
-  .filter((post) => !CONSOLIDATED_BLOG_SLUGS.has(post.slug));
+export const blogPosts = [
+  ...day4ComplianceBlogPosts,
+  ...extraBlogPosts,
+  ...coreBlogPosts,
+].filter((post) => !CONSOLIDATED_BLOG_SLUGS.has(post.slug));
