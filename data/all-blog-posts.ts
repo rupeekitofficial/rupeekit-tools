5e1eb73bbeb8bac725317d9a7400357e087a5ff1
rupeekitofficial <rupeekitofficial@gmail.com>
2026-08-09 09:55:27 +0530

    feat(blog): register day 7 comparison guides

--- data/all-blog-posts.ts
+++ data/all-blog-posts.ts
@@ -1,6 +1,7 @@
 import { blogPosts as coreBlogPosts } from './blog-posts';
 import { extraBlogPosts } from './extra-blog-posts';
 import { day4ComplianceBlogPosts } from './day4-compliance-blog-posts';
+import { day7ComparisonBlogPosts } from './day7-comparison-blog-posts';
 import { CONSOLIDATED_BLOG_SLUGS } from '../lib/consolidated-routes';
 
 export type {
@@ -12,6 +13,7 @@ export type {
 } from './blog-posts';
 
 export const blogPosts = [
+  ...day7ComparisonBlogPosts,
   ...day4ComplianceBlogPosts,
   ...extraBlogPosts,
   ...coreBlogPosts,
